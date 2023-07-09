b832b318cdcd15e69198e5d48dbb953ee2e507f3
guilhermedelucas <gdelucasde@gmail.com>
2023-07-09 11:50:14 +0200

    Math random + add main message on the files module

--- modules/files.py
+++ modules/files.py
@@ -12,3 +12,8 @@ def write_todos(content, filepath=FILEPATH):
     """ Write the to-do items list in the text file """
     with open(filepath, 'w') as file_local:
         file_local.writelines(content)
+
+
+if __name__ == '__main__':
+    print('Hello')
+    print(get_todos())
