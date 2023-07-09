219497d1b29ec2f7d3ba20924ab0d779d5a1ad79
guilhermedelucas <gdelucasde@gmail.com>
2023-07-09 11:51:04 +0200

    Unique main file - without match

--- main.py
+++ main.py
@@ -1,49 +1,57 @@
-from files import get_todos, write_todos
+from modules.files import get_todos, write_todos
+import time
+
+now = time.strftime('%b %d, %Y, %H:%M:%S')
+print('It\'s', now)
 
 while True:
     # Get user input
     user_action = input('Type add, show, edit, complete or exit: ')
 
     # Strip white spaces from the input
-    match user_action.strip():
-        case 'add':
-            todo = input('Enter a todo: ') + "\n"
+    if user_action.startswith('add'):
+        todo = user_action[4:]
 
-            todos = get_todos()
+        todos = get_todos()
+        todos.append(todo + '\n')
 
-            todos.append(todo)
-
-            write_todos(todos)
-        case 'show' | 'display':
-            todos = get_todos()
-            # inline loop
-            # new_todos = [item.strip('\n') for item in todos]
-
-            for i, todo in enumerate(todos):
-                todo = todo.strip('\n')
-                row = f'{i + 1}-{todo.title()}'
-                print(row)
-        case 'edit':
-            number = int(input('Number of todo to edit: '))
-            number = number - 1
+        write_todos(todos)
+    elif user_action.startswith('show'):
+        todos = get_todos()
+        # inline loop
+        # new_todos = [item.strip('\n') for item in todos]
+        for i, todo in enumerate(todos):
+            todo = todo.strip('\n')
+            row = f'{i + 1}-{todo.title()}'
+            print(row)
+    elif user_action.startswith('edit'):
+        try:
+            number = int(user_action[5:]) - 1
 
             todos = get_todos()
             new_todo = input('Enter new todo: ')
             todos[number] = new_todo + '\n'
 
             write_todos(todos)
-        case 'complete':
-            number = int(input('Number of the todo to complete: '))
+        except ValueError:
+            print('Your command is not valid.')
+            continue
+    elif user_action.startswith('complete'):
+        try:
+            number = int(user_action[9:]) - 1
 
             todos = get_todos()
-            todo_to_remove = todos[number - 1].strip('\n')
-            todos.pop(number - 1)
+            todo_to_remove = todos[number].strip('\n')
+            todos.pop(number)
             write_todos(todos)
 
             print(f'Todo {todo_to_remove} was removed from the list.')
-        case 'exit':
-            break
-        case _:
-            print('Hey, you entered an unknown command')
+        except (IndexError, ValueError):
+            print('There\'s no item with that number.')
+            continue
+    elif user_action.startswith('exit'):
+        break
+    else:
+        print('Hey, you entered an unknown command')
 
 print('Bye')
