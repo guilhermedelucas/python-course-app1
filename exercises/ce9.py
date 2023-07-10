456c0a70313adbfcdc208c889d5a9926f82b2177
guilhermedelucas <gdelucasde@gmail.com>
2023-07-10 17:18:50 +0200

    Exercise: GUI convert feet to meter

---
+++ exercises/ce9.py
@@ -0,0 +1,33 @@
+import PySimpleGUI as sg
+from bonus.converters14 import convert as feet_inches_to_meters
+
+label1 = sg.Text('Enter feet: ')
+input1 = sg.Input(key="feet")
+
+label2 = sg.Text('Enter inches: ')
+input2 = sg.Input(key="inches")
+
+convert_button = sg.Button('Convert', key='convert')
+output_text = sg.Text(key="output_text")
+
+window = sg.Window('Convertor', layout=[
+    [label1, input1],
+    [label2, input2],
+    [convert_button, output_text]
+])
+
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+
+    try:
+        feet = int(values['feet'])
+        inches = int(values['inches'])
+        meters = feet_inches_to_meters(feet, inches)
+        window['output_text'].update(value=f"{'{:.2f}'.format(round(meters, 2))}m", text_color="white")
+    except ValueError:
+        window['output_text'].update(value='Invalid values')
+
+window.close()
