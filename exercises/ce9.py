ae8549a55cb50a683b4f927d432866ffb783c8e7
guilhermedelucas <gdelucasde@gmail.com>
2023-07-10 17:48:21 +0200

    Code exercise: added theme and exit button into the Convertor app

--- exercises/ce9.py
+++ exercises/ce9.py
@@ -1,6 +1,9 @@
 import PySimpleGUI as sg
 from bonus.converters14 import convert as feet_inches_to_meters
 
+
+sg.theme('Black')
+
 label1 = sg.Text('Enter feet: ')
 input1 = sg.Input(key="feet")
 
@@ -8,18 +11,22 @@ label2 = sg.Text('Enter inches: ')
 input2 = sg.Input(key="inches")
 
 convert_button = sg.Button('Convert', key='convert')
+exit_button = sg.Button('Exit', key="exit")
 output_text = sg.Text(key="output_text")
 
 window = sg.Window('Convertor', layout=[
     [label1, input1],
     [label2, input2],
-    [convert_button, output_text]
+    [convert_button, exit_button, output_text]
 ])
 
 
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
+        break
+
+    if event == 'exit':
         break
 
     try:
